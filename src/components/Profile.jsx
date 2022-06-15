a32d92617b48c3c455ad471a265f7140ffc04618
SidathDabare <sidath2007@gmail.com>
2022-06-15 11:08:12 +0200

    file-structure-setup

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import "../style/Profile.css"
+
+const Profile = () => {
+    return (
+        <Container className='profile-container'>Profile</Container>
+    )
+}
+
+export default Profile
