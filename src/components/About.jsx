7ffc727783b3164fae04befbfff953c8d0dde45c
SidathDabare <sidath2007@gmail.com>
2022-06-15 13:29:35 +0200

    profile component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "../style/About.css"
 
 const About = () => {
     return (
-        <div className='about'>About</div>
+        <div className='about d-flex p-2'>
+            <div>
+                <p><Link to="/">Group</Link></p>
+                <p><Link to="/">Events</Link></p>
+                <p><Link to="/">Followed Hashtags</Link></p>
+            </div>
+            <div className='d-flex justify-content-center align-items-center'>
+                <i className="bi bi-plus-lg"></i>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,43 @@ import "../style/Profile.css"
 
 const Profile = () => {
     return (
-        <div className='profile-container'>Profile</div>
+        <div className='profile-container'>
+            <div className='header'>
+                <div className="cover-image-div">
+                    <img className='cover-image' src="https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto/wp-cms/uploads/2021/03/LinkedIn-Default-Background-2020-.jpg" alt="" />
+                </div>
+                <div className="profile-deatails">
+                    <strong>Sidath Dabare</strong>
+                    <p>Web Developer</p>
+                </div>
+                <div className="image-container">
+                    <img className="profile-image" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="" />
+                </div>
+            </div>
+            <div className="section-1 d-flex justify-content-between p-2">
+                <div className="connection-details">
+                    <p>Connection</p>
+                    <p><strong>Grow your network</strong></p>
+                </div>
+                <div className="connection-count">
+                    <strong>12</strong>
+                </div>
+            </div>
+            <div className="section-2 p-2">
+                <div><small>Access exlusive tools & insights</small></div>
+                <div className='d-flex'>
+                    <div><i className="bi bi-app"></i></div>
+                    <div className='ml-2'><strong>Network Smarter, Try Premium Free</strong></div>
+                </div>
+
+            </div>
+            <div className="section-3 p-2">
+                <div className='d-flex'>
+                    <div><i className="bi bi-bookmark-fill"></i></div>
+                    <div className='ml-2'><strong>My items</strong></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
