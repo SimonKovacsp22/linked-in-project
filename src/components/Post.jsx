431c6c04d5d32d5395d9742632211cd14244b812
SimonKovacsp22 <postcardsent@gmail.com>
2022-06-16 14:58:09 +0200

    commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../style/PostList.css"
+
+export default function Post() {
+  return (
+    null
+  )
+}
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "../style/PostList.css"
+import Post from './Post'
 
 const PostList = () => {
     return (
-        <div className='post-list'>PostList</div>
+        <div className='post-list'>
+          <Post/>
+        </div>
     )
 }
 
