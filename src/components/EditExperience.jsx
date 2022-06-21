dddd2467cd7e29f93374006186966cbbac4ca402
farshadBish <farshadota@gmail.com>
2022-06-21 16:52:04 +0200

    update

--- src/components/EditExperience.jsx
+++ src/components/EditExperience.jsx
@@ -17,24 +17,72 @@ const EditExperience = (props) => {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
+            <Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Role</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Role"
+                            value=''
+                            onChange={(e) => (e.target.value)}
+                        />
                     </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Company</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Company Name"
+                            value=''
+                            onChange={(e) => (e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Area</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Name Of City-Country"
+                            value=''
+                            onChange={(e) =>(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>start Date</Form.Label>
+                        <Form.Control
+                            type="date"
+                            placeholder="Started-Date"
+                            value=''
+                            onChange={(e) => (e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>End Date</Form.Label>
+                        <Form.Control
+                            type="date"
+                            placeholder="Finish-Date"
+                            value=''
+                            onChange={(e) =>(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Desc"
+                            value=''
+                            onChange={(e) => (e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Add image</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value=''
+                            onChange={(e) => (e.target.value)}
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
+                    <Button type="submit" variant="primary" onClick={''} className='mb-3'>
+                        Save
                     </Button>
                 </Form>
             </Modal.Body>
