2f4ec59d317c5bdcf3e76aaf50acbef3b6186bf0
farshadBish <farshadota@gmail.com>
2022-08-26 12:23:56 +0200

    update

--- src/components/SearchItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "../style/Navbar.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-export default function SearchItem({firstName,surname}) {
-  return (
-    <div  className='navbar-search-item d-flex align-items-center'><FontAwesomeIcon icon={faSearch}/><p style={{margin:0}}>{firstName} &nbsp;{surname}</p></div>
-  )
-}
